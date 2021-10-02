0ffe92053811a6faeabc994c00979954ff1ca223
Arseniy16 <arseniyi16.11@gmail.com>
2021-10-02 05:45:28 +0300

    exec

--- exec/exec.c
+++ exec/exec.c
@@ -7,60 +7,60 @@
 
 double time_ms(struct timeval start, struct timeval stop)
 {
-	double sec1 = (stop.tv_usec - start.tv_usec)/1000000.0;
-	double sec2 = stop.tv_sec - start.tv_sec;
-	return (sec1 + sec2) * 1000.0;
+    double sec1 = (stop.tv_usec - start.tv_usec)/1000000.0;
+    double sec2 = stop.tv_sec - start.tv_sec;
+    return (sec1 + sec2) * 1000.0;
 }
 
 int main(int argc, char const *argv[])
 {
-	pid_t pid = 0;	
-	int status = 0;
+    pid_t pid = 0;  
+    int status = 0;
 
-	struct timeval start = {0, 0};
-	struct timeval stop = {0, 0};
+    struct timeval start = {0, 0};
+    struct timeval stop = {0, 0};
 
-	if (argc == 1)
-	{
-		printf("Write more argumets\n");
-		return -1;
-	}
-	else
-	{
-		pid = fork();
+    if (argc == 1)
+    {
+        printf("Write more argumets\n");
+        return -1;
+    }
+    else
+    {
+        pid = fork();
 
-		if (gettimeofday(&start, NULL) == -1)
-		{
-			perror("ERROR to get time start");
-			return -2;
-		}
+        if (gettimeofday(&start, NULL) == -1)
+        {
+            perror("ERROR to get time start");
+            return -2;
+        }
 
-		if (pid == -1)
-		{
-			perror("ERROR to fork");
-			return -1;
-		}
-		else if (pid == 0)
-		{
-			if (execvp(argv[1], argv+1) == -1)
-			{
-				perror("ERROR to exec file");
-				return -1;
-			}			
-		}
-		else
-		{
-			wait(&status);
-		}	
+        if (pid == -1)
+        {
+            perror("ERROR to fork");
+            return -1;
+        }
+        else if (pid == 0)
+        {
+            if (execvp(argv[1], argv+1) == -1)
+            {
+                perror("ERROR to exec file");
+                return -1;
+            }           
+        }
+        else
+        {
+            wait(&status);
+        }   
 
-		if (gettimeofday(&stop, NULL) == -1)
-		{
-			perror("ERROR to get time stop");
-			return -2;
-		}						
-	}
+        if (gettimeofday(&stop, NULL) == -1)
+        {
+            perror("ERROR to get time stop");
+            return -2;
+        }                       
+    }
 
-	printf("TIME: %.3lg ms\n", time_ms(start, stop));
+    printf("TIME: %.3lg ms\n", time_ms(start, stop));
 
-	return 0;
+    return 0;
 }
