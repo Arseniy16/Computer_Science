7cf1334c3057a7b8072715357cd60a719b7ccd29
Arseniy16 <arseniyi16.11@gmail.com>
2022-05-16 21:25:40 +0300

    new

--- 4_semestr/Snake/gview.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "gview.h"
-
-int GView::print_value(int x, int y, int z)
-{
-    printf("GX = %d, GY = %d, GZ = %d\n", x, y, z);
-    return 0;
-}
--- 4_semestr/Snake/tview.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "tview.h"
-
-int TView::print_value(int x, int y, int z)
-{
-    printf("TX = %d, TY = %d, TZ = %d\n", x, y, z);
-    return 0;
-}
